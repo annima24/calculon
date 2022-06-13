8af6a8630a4effecc5231ed547ee6b1ee0da31d0
annima24 <nm.andrew24@gmail.com>
2022-06-12 20:07:24 -0400

    make the calculator add

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const display = document.querySelector(".calculator-display");
+const equals = document.querySelector(".equals");
+let computation = [];
+let currentOperator;
 
-let buttons = document.querySelectorAll('button')
-
-function add(a,b)  {
-    return a + b;
+function add(a, b) {
+  return a + b;
 }
 
-function subtract(a,b)  {
-    return a - b;
+function subtract(a, b) {
+  return a - b;
 }
 
-function multiply(a,b)  {
-    return a * b;
+function multiply(a, b) {
+  return a * b;
 }
 
-function divide(a,b)  {
-    return a / b;
+function divide(a, b) {
+  return a / b;
 }
 
-function operate(operator, a, b)    {
-    return operator(a,b)
+function operate(operator, a, b) {
+  return operator(a, b);
 }
 
-buttons.forEach(button => button.addEventListener('click', buttonOperator))
+//listens to all the buttons on the calculator
+numbers.forEach((number) => number.addEventListener("click", numberHandler));
+operators.forEach((operator) => operator.addEventListener("click", operatorHandler));
+equals.addEventListener('click', equalsHandler);
 
-function buttonOperator(event)   {
-    console.log(event.target.textContent)
+function numberHandler(event)    {
+    if (display.textContent === '+')    {
+        display.textContent = '';
+        display.textContent += event.target.textContent;    
+    }else display.textContent += event.target.textContent;
+}
+
+function operatorHandler(event)    {
+    switch(event.target.textContent)    {
+        case '+':
+            computation.push(add);
+            computation.push(display.textContent);
+            display.textContent = '+';
+            break;
+    }
+}
+
+function equalsHandler()    {
+    computation.push(display.textContent);
+    display.textContent = operate(computation[0], Number(computation[1]), Number(computation[2]));
 }
 
 
 
+function handleOperator(event) {
+  computation.push(display.textContent);
+  computation.push(currentOperator);
+  display.textContent = event.target.textContent;
+}
